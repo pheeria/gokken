96493d33d5104a2e04f4e913c2618eac4cd09649
pheeria <olzhas.askar@deliveryhero.com>
2021-01-10 17:16:53 +0100

    feat: quick sort

--- quick_sort.go
+++ quick_sort.go
@@ -1,21 +1,74 @@
 package gokken
 
 func QuickSort(sequence []int) []int {
-    if len(sequence) < 2 {
-        return sequence
-    }
+	if len(sequence) < 2 {
+		return sequence
+	}
 
-    pivot := sequence[0]
-    var less []int
-    var greater []int
+	pivot := sequence[0]
+	var less []int
+	var greater []int
 
-    for _, v := range sequence[1:] {
-        if v <= pivot {
-            less = append(less, v)
-        } else {
-            greater = append(greater, v)
-        }
-    }
+	for _, v := range sequence[1:] {
+		if v <= pivot {
+			less = append(less, v)
+		} else {
+			greater = append(greater, v)
+		}
+	}
 
-    return append(append(QuickSort(less), pivot), QuickSort(greater)...)
+	return append(append(QuickSort(less), pivot), QuickSort(greater)...)
+}
+
+func swap(sequence []int, a, b int) {
+	tmp := sequence[a]
+	sequence[a] = sequence[b]
+	sequence[b] = tmp
+}
+
+func partitionLomuto(sequence []int, low, high int) int {
+	pivot := sequence[high]
+	i := low
+	for j := low; j < high; j++ {
+		if sequence[j] < pivot {
+			swap(sequence, i, j)
+			i++
+		}
+	}
+	swap(sequence, i, high)
+	return i
+}
+func QuickSortLomuto(sequence []int, low, high int) {
+	if low < high {
+		p := partitionLomuto(sequence, low, high)
+		QuickSortLomuto(sequence, low, p-1)
+		QuickSortLomuto(sequence, p+1, high)
+	}
+}
+
+func partititionHoare(sequence []int, low, high int) int {
+	pivot := (low + high) / 2
+	left := low
+	right := high
+	for {
+		for sequence[left] < sequence[pivot] {
+			left++
+		}
+		for sequence[right] > sequence[pivot] {
+			right--
+		}
+
+		if left >= right {
+			return right
+		}
+
+		swap(sequence, left, right)
+	}
+}
+func QuickSortHoare(sequence []int, low, high int) {
+	if low < high {
+		pivot := partititionHoare(sequence, low, high)
+		QuickSortHoare(sequence, low, pivot)
+		QuickSortHoare(sequence, pivot+1, high)
+	}
 }
--- quick_sort_test.go
+++ quick_sort_test.go
@@ -1,14 +1,36 @@
 package gokken
 
-import "testing"
-import "reflect"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestQuickSort(t *testing.T) {
 	input := []int{5, 4, 3, 2, 1}
 	want := []int{1, 2, 3, 4, 5}
-    got := QuickSort(input)
+	got := QuickSort(input)
 
-    if !reflect.DeepEqual(got, want) {
-        t.Errorf("got %q want %q", got, want)
-    }
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestQuickSortLomuto(t *testing.T) {
+	got := []int{5, 4, 3, 2, 1}
+	want := []int{1, 2, 3, 4, 5}
+	QuickSortLomuto(got, 0, len(got)-1)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestQuickSortHoare(t *testing.T) {
+	got := []int{5, 4, 3, 2, 1}
+	want := []int{1, 2, 3, 4, 5}
+	QuickSortHoare(got, 0, len(got)-1)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
