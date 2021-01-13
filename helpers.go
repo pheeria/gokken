7924ab9d2083d622004028477ccf49632465207d
pheeria <olzhas.askar@deliveryhero.com>
2021-01-13 23:00:20 +0100

    feat: test arrays

---
+++ helpers.go
@@ -0,0 +1,39 @@
+package gokken
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+func createRandomIntArray(size int) []int {
+	var result []int
+	rand.Seed(int64(size))
+
+	for size >= len(result) {
+        result = append(result, rand.Intn(size * 10))
+	}
+
+	return result
+}
+
+func sortAsNew(sequence []int) []int {
+    result := make([]int, len(sequence))
+    copy(result, sequence)
+    sort.Ints(result)
+    return result
+}
+
+func CreateTestArrays(size int) (sorted, unsorted []int) {
+    unsorted = createRandomIntArray(size)
+    sorted = sortAsNew(unsorted)
+    return
+}
+
+
+func main() {
+    sorted, unsorted := CreateTestArrays(10)
+
+    fmt.Println(sorted)
+	fmt.Println(unsorted)
+}
