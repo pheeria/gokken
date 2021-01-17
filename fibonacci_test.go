a7780d61e0ab35bb53a8ca797230516bfa7aa139
pheeria <olzhas.askar@deliveryhero.com>
2021-01-17 14:06:18 +0100

    feat: fibonacci

---
+++ fibonacci_test.go
@@ -0,0 +1,32 @@
+package gokken
+
+import (
+    "testing"
+    "reflect"
+)
+
+var want = []int {0, 1, 1, 2, 3, 5, 8, 13, 21, 34}
+
+func TestIterativeFibonacci(t *testing.T) {
+    got := IterativeFibonacci(10)
+
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got %v want %v", got, want)
+    }
+}
+
+func TestRecursiveFibonacci(t *testing.T) {
+    got := RecursiveFibonacci(10)
+
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got %v want %v", got, want)
+    }
+}
+
+func TestClosureFibonacci(t *testing.T) {
+    got := ClosureFibonacci(10)
+
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got %v want %v", got, want)
+    }
+}
