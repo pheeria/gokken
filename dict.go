044758815d7d003e2fcb9ecc2e7b51b5d2bc15c1
pheeria <olzhas.askar@deliveryhero.com>
2021-01-19 19:36:22 +0100

    feat: primitive dict

---
+++ dict.go
@@ -0,0 +1,37 @@
+package gokken
+
+type Dict struct {
+    keys []string
+    values []string
+    size int
+}
+func (d *Dict) hash(key string) int {
+    return len(key)
+}
+func (d *Dict) Init() {
+    d.keys = make([]string, d.size, d.size * 2)
+    d.values = make([]string, d.size, d.size * 2)
+}
+func (d *Dict) Set(key, value string) {
+    hashed := d.hash(key)
+    d.keys[hashed] = key
+    d.values[hashed] = value
+}
+func (d *Dict) Get(key string) string {
+    hashed := d.hash(key)
+    return d.values[hashed]
+}
+
+func (d Dict) String() string {
+    result := "{\n"
+
+    for i, v := range d.keys {
+        if v != "" {
+            result += "  " + v + ": " + d.values[i] + "\n"
+        }
+    }
+    result += "}"
+
+    return result
+}
+
---
+++ dict_test.go
@@ -0,0 +1,18 @@
+package gokken
+
+import "testing"
+
+func TestDict(t *testing.T) {
+    dict := Dict{ size: 10 }
+    dict.Init()
+    dict.Set("first", "word")
+    dict.Set("second", "palabra")
+
+    got := dict.Get("second")
+    want := "palabra"
+
+    if got != want {
+        t.Errorf("got %s want %s", got, want)
+    }
+}
+
