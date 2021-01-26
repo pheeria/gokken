744ee91cc4c0fd1da3125c60a6cb33a22652e6ed
pheeria <olzhas.askar@deliveryhero.com>
2021-01-26 23:17:44 +0100

    feat: queue

---
+++ queue.go
@@ -0,0 +1,22 @@
+package gokken
+
+import "strings"
+
+type Queue struct {
+    head, tail int
+    items []string
+}
+
+func (q *Queue) Enqueue(item string) {
+    q.items = append(q.items, item)
+    q.tail++
+}
+
+func (q *Queue) Dequeue() string {
+    q.head++
+    return q.items[q.head - 1]
+}
+
+func (q Queue) String() string {
+    return strings.Join(q.items[q.head:q.tail], ", ")
+}
---
+++ queue_test.go
@@ -0,0 +1,27 @@
+package gokken
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestQueue(t *testing.T) {
+    queue := Queue{}
+    queue.Enqueue("first")
+    queue.Enqueue("second")
+    queue.Enqueue("third")
+
+    if queue.Dequeue() != "first" {
+        t.Error("Didn't remove from the beginning!")
+    }
+    if fmt.Sprint(queue) != "second, third" {
+        t.Error("Didn't remove from the beginning!")
+    }
+
+    if queue.Dequeue() != "second" {
+        t.Error("Didn't remove from the beginning!")
+    }
+    if fmt.Sprint(queue) != "third" {
+        t.Error("Didn't remove from the beginning!")
+    }
+}
